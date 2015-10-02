a3123e1ebace7d9cb2f368cce111168b3c684ce7
Carlos <Carlos@Tay-Topstar.pmr.lan>
2015-10-02 21:08:17 +0100

    Third commit

--- src/moneycalculator/MoneyCalculator.java
+++ src/moneycalculator/MoneyCalculator.java
@@ -3,7 +3,12 @@ package moneycalculator;
 public class MoneyCalculator {
 
     public static void main(String[] args) {
+        Currency dollar = new Currency("USD","American Dollar","$");
+        Currency euro = new Currency("EUR","Euro","€");
+        ExchangeRate rate = new ExchangeRate(euro, dollar, 1.20);
+        Money amount = new Money(100.9,dollar);
 
+        System.out.println(MoneyExchanger.exchange(amount,rate).getAmount());
     }
     
 }
--- src/moneycalculator/MoneyExchanger.java
+++ src/moneycalculator/MoneyExchanger.java
@@ -1,7 +1,7 @@
 package moneycalculator;
 
 public class MoneyExchanger {
-    public Money exchange(Money money, ExchangeRate exchangeRate){
-        return null;
+    public static Money exchange(Money money, ExchangeRate exchangeRate){
+        return new Money(money.getAmount() * exchangeRate.getRate(),exchangeRate.getTo());
     }
 }
